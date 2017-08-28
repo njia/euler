85bdb87084f428153b42bcc24a368bc14e8f2b08
Nan Jia <jiananmail@gmail.com>
2017-08-28 16:19:20 +1000

    problem 14 improvement, run time was 14 seconds now under 3

--- perl/14.pl
+++ perl/14.pl
@@ -1,30 +1,33 @@
 #!/usr/bin/env perl
-#or /usr/bin/perl if you don't have perlbrew
 
 use warnings;
 use strict;
 
-my $count = 0;
-my $result = 0;
+my $count   = 0;
+my $result  = 0;
 my $max_seq = 0;
-my $t = 0;
+my $t       = 0;
+my %cache;
 
-sub collatz {
-  $t = $_[0];
-  while($t > 1) {
+my $number = 0;
+
+foreach $number (2 .. 1000000) {
+  $t = $number;
+  while ($t > 1) {
     $count += 1;
-    if ($t % 2 == 0) {
+    if ($cache{$t}) {
+      $count += $cache{$t};
+      last;
+    }
+    if ($t %2 == 0){
       $t = $t/2;
     } else {
       $t = $t*3 + 1;
     }
   }
-}
 
-my $number = 0;
+  $cache{$number} = $count;
 
-foreach $number (2 .. 1000000) {
-  &collatz($number);
   if ($count > $max_seq) {
     $max_seq = $count;
     $result  = $number;
